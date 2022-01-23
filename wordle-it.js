0a2541948baf9188b305371eacf20be160ac9079
Edoardo <edoardo.vr96@hotmail.it>
2022-01-23 12:29:44 +0100

    Update wordle-it.js

--- wordle-it.js
+++ wordle-it.js
@@ -1044,7 +1044,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
   var Qa = "IN_PROGRESS",
       Za = "WIN",
       es = "FAIL",
-      as = ["Genio!!!", "Magnifico", "Notevole", "Ottimo", "Non male", "Daje!"],
+      as = ["Baro o fortunato?", "Magnifico", "Notevole", "Ottimo", "Non male", "Per un pelo!"],
       ss = function(e) {
           n(t, e);
           var a = h(t);
